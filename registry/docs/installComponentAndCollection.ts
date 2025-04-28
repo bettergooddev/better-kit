84fb8c7bff49b50616bac6161597366c95ba682c
bettergooddev <dev@bettergood.agency>
2025-04-28 13:03:51 -0400

    updated install instructions

--- registry/docs/installComponentAndCollection.ts
+++ registry/docs/installComponentAndCollection.ts
@@ -13,9 +13,10 @@ const message = ({
 }) =>
   `Cursor instructions: \n` +
   `1. Visit src/collections/Pages/index.ts and add 'import { ${collectionName} } from '@/blocks/${collectionFolderName}/config'.  Then, add ${collectionName} into the list of blocks found at Pages.fields[1].tabs[1].fields[0].blocks. \n` +
-  `2. Visit src/blocks/RenderBlocks.tsx and add \`${blockComponentName}: ${blockComponentFolder}\` to the blockComponents object. \n` +
-  `3. Run 'npx payload generate:types'. \n` +
-  `4. (Optional) Run 'tsc --build' to ensure everything looks good in regards to ${collectionName}, and ${blockComponentName}. \n`
+  `2. Visit src/payload.config.ts and add 'import { ${collectionName} } from './collections/${collectionFolderName}'.  Then, add ${collectionName} into the buildConfig.collections array. \n` +
+  `3. Visit src/blocks/RenderBlocks.tsx and add \`${blockComponentName}: ${blockComponentFolder}\` to the blockComponents object. \n` +
+  `4. Run 'npx payload generate:types'. \n` +
+  `5. (Optional) Run 'tsc --build' to ensure everything looks good in regards to ${collectionName}, and ${blockComponentName}. \n`
 
 
 
